faeb5a109467db88499ad0d30954fe8591f29988
thicorrea1 <thicorrea1@gmail.com>
2013-08-11 03:21:54 -0300

    Inicio do Projeto

---
+++ MSEP/Controllers/PlanosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSEP.Controllers
+{
+    public class PlanosController : Controller
+    {
+        //
+        // GET: /Planos/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Upgrade()
+        {
+            return View();
+        }
+    }
+}
---
+++ MSEP/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSEP.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string tipo { get; set; }
+        public string endereco { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string cidade { get; set; }
+        public string uf { get; set; }
+        public string cep { get; set; }
+        public string pais { get; set; }
+        public string apelido { get; set; }
+        public DateTime cadastro { get; set; }
+        public bool ativo { get; set; }
+    }
+}
